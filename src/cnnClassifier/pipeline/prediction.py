328999948f025ccef657a7f744f81be94d6a795c
Divyadarshan Srivastava <divyadarshan.dsa@gmail.com>
2026-09-18 01:05:22 +0530

    prediction.py updated

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -4,27 +4,50 @@ from tensorflow.keras.preprocessing import image
 import os
 
 
-
 class PredictionPipeline:
-    def __init__(self,filename):
-        self.filename =filename
 
+    def __init__(self, filename):
+        self.filename = filename
 
-    
     def predict(self):
-        # load model
-        model = load_model(os.path.join("model", "model.h5"))
 
+        # Load trained model
+        model_path = os.path.join("artifacts", "training", "model.h5")
+
+        model = load_model(
+            model_path,
+            compile=False
+        )
+
+        # Load uploaded image
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
-        test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
 
+        test_image = image.load_img(
+            imagename,
+            target_size=(224, 224)
+        )
+
+        # Convert image to numpy array
+        test_image = image.img_to_array(test_image)
+
+        # Add batch dimension
+        test_image = np.expand_dims(
+            test_image,
+            axis=0
+        )
+
+        # Make prediction
+        result = np.argmax(
+            model.predict(test_image),
+            axis=1
+        )
+
+        print("Prediction result:", result)
+
+        # Class prediction
         if result[0] == 1:
-            prediction = 'Tumor'
-            return [{ "image" : prediction}]
+            prediction = "Tumor"
         else:
-            prediction = 'Normal'
-            return [{ "image" : prediction}]
+            prediction = "Normal"
+
+        return [{"image": prediction}]
